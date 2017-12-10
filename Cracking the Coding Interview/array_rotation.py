4fc0623c6f5e7bcf40bf38ce6c5b5d0916d5606d
arkwl <a.rckwll@gmail.com>
2017-12-09 22:40:03 -0500

    better solution lol

--- Cracking the Coding Interview/array_rotation.py
+++ Cracking the Coding Interview/array_rotation.py
@@ -1,24 +1,16 @@
-def array_left_rotation(a, n, k):    
-    array = a
-    if(n < 2):
-        return array
-    while(k > 0):
-        last = array[n-1] #last element
-        tmp = 0;
-        for x in range(0, n):
-            #print(str(x)+"    "+str(n))
-            index = abs(x - n)
-            #print(str(index))
-            tmp = array[index - 1]
-            #print("last: " +str(last)+"temp: "+str(tmp)+" index: "+ str(index))
-            array[index - 1] = last
-            last = tmp
-            if(index - 1 == 0):
-                # copy last to n
-                array[n-1]= last
-                break;
-        k = k - 1;
+def rotate(array):
+    temp = array[0]
+    del array[0]
+    array.append(temp)
     return array
+
+def array_left_rotation(a, n, k):
+    array = a
+    rotations = k % n
+    for i in range(rotations):
+        array = rotate(array)
+    return array
+
 
 n, k = map(int, input().strip().split(' '))
 a = list(map(int, input().strip().split(' ')))
